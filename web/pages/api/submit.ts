b1606b3430437b9d9d2d6a17d129f7ec8f6aeca9
Nicholas Osaka <nicholas.osaka@gmail.com>
2022-11-04 17:44:44 -0400

    Fix submission

--- web/pages/api/submit.ts
+++ web/pages/api/submit.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Airtable from 'airtable'
+import nodemailer from 'nodemailer'
 
 let base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE!);
 
@@ -19,24 +20,57 @@ export default async function handler(
 ) {
     const body = req.body
     console.log(`Form submission received`)
-    try {
-        const records = await base('Submissions').create([
-            {
-                "fields": {
-                    "Abstract": body.abstract,
-                    "Keywords": body.keywords,
-                    "Name": body.name,
-                    "Email": body.email,
-                    "Institution": body.institution,
-                    "Student Status": body.studentstatus,
-                    "Title": body.title
-                }
+    base('Submissions').create([
+        {
+            "fields": {
+                "Abstract": body.abstract,
+                "Keywords": body.keywords,
+                "Name": body.name,
+                "Email": body.email,
+                "Institution": body.institution,
+                "Student Status": body.studentstatus,
+                "Title": body.title
+            }
+        },
+    ]).then(async (records) => {
+        console.log(`ID: ${records[0].getId()}`);
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.EMAIL_USER, // generated ethereal user
+                pass: process.env.EMAIL_PASS, // generated ethereal password
             },
-        ]);
-        console.log("Success");
-        return res.redirect('/cfp/confirmation');
-    } catch (err) {
-        console.log(`Error: ${err}`);
-        return res.redirect('/cfp/failure');
-    }
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: {
+                name: "Ethics and Applied Philosophy Graduate Conference",
+                address: process.env.EMAIL_USER!
+            },
+            to: body.email, // list of receivers
+            subject: "Submission Confirmation for the Ethics and Applied Philosophy Graduate Conference", // Subject line
+            text:
+                `This is a receipt of submission for the Ethics and Applied Philosophy Graduate Conference
+
+            Name: ${body.name}
+            Email: ${body.email}
+            Institution: ${body.institution}
+            Student Status: ${body.studentstatus}
+            Paper Title: ${body.title}
+            
+            Your abstract and keywords are ommitted for brevity.
+            
+            Thank you for your submission!`,
+        });
+
+        console.log("Message sent: %s", info.messageId);
+
+    }).catch(e => console.error(e))
+
+    res.redirect('/cfp/confirmation')
 }
--- web/pages/cfp/confirmation.tsx
+++ web/pages/cfp/confirmation.tsx
@@ -11,8 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <main className='container w-10/12 mx-auto my-20'>
-        <p className='flex justify-center'>Thank you for your submission. Please check the email provided for confirmation of submission.</p>
+      <main className='container w-10/12 md:w-1/2 mx-auto my-20'>
+        <p className='flex justify-center'>Thank you for your submission. Please check the email provided for confirmation of submission. If you do not receive a confirmation of submission within 24 hours, please contact one of the conference organizers.</p>
       </main>
     </div>
   )
