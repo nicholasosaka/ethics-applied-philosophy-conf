78ee6b64a60bd8a10d173e3a756e318a23fbfa8e
Nicholas Osaka <nicholas.osaka@gmail.com>
2024-05-01 12:58:56 -0400

    Update index.tsx

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home() {
         <Program/>
         <h1 className='text-2xl font-bold text-center py-5'>Notes and Acknowledgements</h1>
         <p className='py-2'>The Philosophical Union of UNC Charlotte is the graduate student organization of students at UNC Charlotte interested in philosophical interventions and discussion. Annually since 2018, the Philosophical Union has organized the Ethics and Applied Philosophy Graduate Conference, a home for philosophical interventions and discourse from an applied and interdisciplinary perspective.</p>
+        <p className='py-2'>This conference is funded in part by the Center for Professional & Applied Ethics at the University of North Carolina at Charlotte. The Center does not necessarily endorse the beliefs or actions of this organization.</p>
         <p className='py-2'>This conference is funded in part by the Graduate & Professional Student Government (GPSG) of the University of North Carolina at Charlotte with student activity fees. The GPSG and Student Activity Fees Commision do not necessarily endorse the beliefs or actions of this organization.</p>
         <div className='flex justify-center py-10'>
           <Image src="/gpsgdisclosure.png" width={400} height={200} alt={'Funded in part by the Graduate & Professional Student Government with your student activity fees. GPSG & SAFC do not necessarily endorse the beliefs or actions of this organization.'}/>
