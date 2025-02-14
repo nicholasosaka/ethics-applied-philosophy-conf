d9ccab492bfd7b5b44f28c587a49e8a80664a8f6
bdavi182 <bdavi182@charlotte.edu>
2025-02-14 12:50:06 -0500

    Update Program.tsx

--- web/components/Program.tsx
+++ web/components/Program.tsx
@@ -9,17 +9,17 @@ export default function Home() {
   <main className='mx-auto my-5'>
       <div className='text-center mb-10 flex xl:flex-row flex-col xl:gap-x-32 lg:w-2/3 lg:mx-auto'>
         <div>
-          <h2 className='text-5xl font-bold uppercase my-5 xl:my-0 text-left'>Praxis as First Philosophy</h2>
+          <h2 className='text-5xl font-bold uppercase my-5 xl:my-0 text-left'>Ethics in a Changing World</h2>
         </div>
         <div className='text-left flex flex-row'>
           <div className="inline-block h-50 xl:w-6 self-stretch opacity-100 bg-jet xl:mr-8 xl:mt-1"></div>
           <div className=''>
-            <h2 className='text-xl font-normal'>6<sup>th</sup> Annual Ethics and Applied Philosophy Graduate Conference hosted by the Philosophical Union of the University of North Carolina at Charlotte</h2>
-            <h2 className='text-xl font-normal pt-4'>April 26 - 28, 2024, virtually</h2>
+            <h2 className='text-xl font-normal'>7<sup>th</sup> Annual Ethics and Applied Philosophy Graduate Conference hosted by the Philosophical Union of the University of North Carolina at Charlotte</h2>
+            <h2 className='text-xl font-normal pt-4'>April 25 - 26, 2025, virtually</h2>
             <h3 className='text-lg font-bold pt-4'>Keynote speakers:</h3>
             <div className='text-lg font-normal'>
-              <h4>Kim Q. Hall, Appalachian State University</h4>
-              <h4>Kathleen Lowenstein, Michigan State University</h4>
+              <h4>Amelia Wiercioch, M.A., University of North Carolina Charlotte Alumna Speaker</h4>
+              <h4>Dr. Evie Kendal, Swinburne University of Technology</h4>
             </div>
           </div>
         </div>
@@ -27,59 +27,59 @@ export default function Home() {
 
       <div className='text-center py-5'>
         <h1 className='text-3xl font-bold pb-2'>Zoom Information</h1>
-        <Link className='underline underline-offset-4' href={'https://charlotte-edu.zoom.us/j/95627127695'}>https://charlotte-edu.zoom.us/j/95627127695</Link>
+        <Link className='underline underline-offset-4' href={'https://charlotte-edu.zoom.us/j/95627127695'}>https://charlotte-edu.zoom.us/j/94641893355?pwd=mhMiBRVcIujaG6PGS0ddvVu3ujCJsg.1</Link>
         <p className='pt-2'>You will need to log into a Zoom account to join. If you are affilitated with UNC Charlotte, please use your university Zoom account.</p>
       </div>
 
       <div className='py-5'>
         <div className='text-center'>
           <h1 className='text-4xl font-bold'>Program</h1>
-          <p className='underline'>Note: all times are in Eastern Daylight Time (GMT-4).</p>
+          <p className='underline'>Note: all times are in Eastern Daylight Time (GMT-4) (UTC-5).</p>
         </div>
 
         <div className='py-5'>
-          <h1 className='text-3xl font-bold pb-2'>Day 1 Schedule (April 26)</h1>
+          <h1 className='text-3xl font-bold pb-2'>Day 1 Schedule (April 25)</h1>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Opening Remarks (1:15pm - 1:30pm ET)</h2>
+            <h2 className='text-1xl font-bold'>Opening Remarks (1:00pm - 1:30pm)</h2>
             <div className='pl-10'>
-              <p>Opening remarks delivered by Philosophical Union of UNC Charlotte Co-Presidents</p>
-              <p>Steve Muir and Nicholas Osaka</p>
+              <p>Opening remarks delivered by Philosophical Union of UNC Charlotte President</p>
+              <p>Brit Davis</p>
             </div>
           </div>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Established Scholar Keynote (1:30pm - 3:00pm ET) — <i>Toward a Cripistemology of Eco-Anxiety</i></h2>
+            <h2 className='text-1xl font-bold'>Alumna Keynote (1:35pm - 2:55pm) — <i>TBA</i></h2>
             <div className='pl-10'>
-              <p>Keynote: Kim Q. Hall, PhD. Professor of Philosophy @ Appalachian State University.</p>
-              <p>Moderator: Billie Waller</p>
+              <p>Keynote: Amelia Wiercioch, M.A. PhD Student @ University of Central Florida.</p>
+              <p>Moderator: TBA</p>
             </div>
           </div>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Panel A (3:00pm - 4:45pm ET) — <i>Topics in Disability Studies</i></h2>
+            <h2 className='text-1xl font-bold'>Panel A (3:00pm - 4:45pm ET) — <i>TBA</i></h2>
             <div className='pl-10'>
-              <p>Speaker: Casey Doherty (Independent): <q>Reimagining Protest: Artivism in the Myalgic Encephalomyelitis (ME) Community</q></p>
-              <p>Speaker: Rosalie Looijaard (Boston University): <q>Against Disability Generalism: Navigating Chronic Pain Through Models of Disability</q></p>
-              <p>Speaker: Kilah Tabatsky (Siena College): <q>Defining Disability Through the Lens of Solidarity</q></p>
-              <p>Moderator: Amanda Hassard</p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Moderator: TBA</p>
             </div>
           </div>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Panel B (4:50pm - 6:20pm ET) — <i>Theorizing Action</i></h2>
+            <h2 className='text-1xl font-bold'>Panel B (4:50pm - 6:20pm ET) — <i>TBA</i></h2>
             <div className='pl-10'>
-              <p>Speaker: Corey McKibbin (Carleton University): <q>Decolonizing Time</q></p>
-              <p>Speaker: Simon Francesco Di Rupo (Università degli Studi di Perugia): <q>The importance of interfaith dialogue in approaching environmental ethics. Raimon Panikkar and Pope Francis in comparison</q></p>
-              <p>Speaker: Scott Ryan Maybell (Independent): <q>Brandom in the Wild: Recognitive Analysis and Recollective Praxis</q></p>
-              <p>Moderator: Pennda Arami</p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Moderator: TBA</p>
             </div>
           </div>
 
         </div>
 
         <div className='py-5'>
-          <h1 className='text-3xl font-bold pb-2'>Day 2 Schedule (April 27)</h1>
+          <h1 className='text-3xl font-bold pb-2'>Day 2 Schedule (April 26)</h1>
 
           <div className="py-5">
             <h2 className='text-1xl font-bold'>Saturday Open (10:45am - 11:00am ET)</h2>
@@ -89,85 +89,44 @@ export default function Home() {
           </div>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Panel C (11:00am - 12:30pm ET) — <i>Specificity in Praxis</i></h2>
+            <h2 className='text-1xl font-bold'>Panel C (11:00am - 12:30pm ET) — <i>TBA</i></h2>
             <div className='pl-10'>
-              <p>Speaker: Emily Lange (Marquette University): <q>Making Better Worlds: Worldbuilding as Feminist Theory and Practice</q></p>
-              <p>Speaker: Dhritiman Barman (Virginia Tech): <q>The Shaping and Reshaping of a Legal Cannabis Market in Southwest Virginia</q></p>
-              <p>Speaker: Jack R Leff (Virginia Tech): <q>On Activist Science</q></p>
-              <p>Moderator: Nicholas Osaka</p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Moderator: TBA</p>
             </div>
           </div>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Panel D (1:20pm - 2:50pm ET) — <i>Affect & Injustice</i></h2>
+            <h2 className='text-1xl font-bold'>Panel D (1:20pm - 2:50pm ET) — <i>TBA</i></h2>
             <div className='pl-10'>
-              <p>Speaker: Roxy Alexander (Independent): <q>Epistemic Injustice and ADHD Girls in the K-12 Education System</q></p>
-              <p>Speaker: Erica Bigelow (University of Washington): <q>Felt Weapons: Wielding Affect to Harm</q></p>
-              <p>Speaker: Triston Hanna (Arizona State University): <q>Degrees of Emotional Fit: A Range View</q></p>
-              <p>Moderator: Knaviyah Howard</p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Speaker: TBA: <q>TBA</q></p>
+              <p>Moderator: TBA</p>
             </div>
           </div>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Alumna Keynote (3:00pm - 4:30pm ET) — <i>Engaging the Margins: Exploring the Intersections of Critical Mental Health and Bioethics</i></h2>
+            <h2 className='text-1xl font-bold'>Established Scholar Keynote (TBA) — <i>TBA</i></h2>
             <div className='pl-10'>
-              <p>Keynote: Kathleen Lowenstein. PhD Candidate, Philosophy, Michigan State University.</p>
-              <p>Moderator: Steve Muir</p>
+              <p>Keynote: Dr, Evie Kendal. Senior Lecturer, Swinburne University of Technology.</p>
+              <p>Moderator: TBA</p>
             </div>
           </div>
 
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Social/Networking Hour (5:05pm - 6:00pm ET)</h2>
+            <h2 className='text-1xl font-bold'>Social/Networking Hour (TBA)</h2>
           </div>
 
         </div>
 
-        <div className='py-5'>
-          <h1 className='text-3xl font-bold pb-2'>Day 3 Schedule (April 28)</h1>
-
           <div className="py-5">
-            <h2 className='text-1xl font-bold'>Sunday Open (10:45am - 11:00am ET)</h2>
+            <h2 className='text-1xl font-bold'>Closing Remarks (TBA)</h2>
             <div className='pl-10'>
-              <p>Sunday Open will consist of coffee chat and technical check.</p>
-            </div>
-          </div>
-
-          <div className="py-5">
-            <h2 className='text-1xl font-bold'>Panel E (11:00am - 12:30pm ET) — <i>Topics in Bioethics</i></h2>
-            <div className='pl-10'>
-              <p>Speaker: Daniel Gaines (Western Michigan University): <q>Transgender Healthcare, Depathologization, and the Gender Dysphoria Diagnosis</q></p>
-              <p>Speaker: Xiu Lim (Independent): <q>Could (some) religious leaders have a special obligation to morally bioenhance? Examining the use of libido-reducing drugs to promote celibacy</q></p>
-              <p>Speaker: Natalia Nealon (University of California, Irvine): <q>Epistemic Shortcuts and Unjust Diagnostic Practices</q></p>
-              <p>Moderator: Steve Muir</p>
-            </div>
-          </div>
-
-          <div className="py-5">
-            <h2 className='text-1xl font-bold'>Panel F (1:20pm - 2:50pm ET) — <i>Feminist Perspectives</i></h2>
-            <div className='pl-10'>
-              <p className='line-through'>Speaker: Margaret Murphy (American University): <q>Nara Smith: A Case Study in Black Motherhood and Trad Wives</q></p>
-              <p>Speaker: Mary Peterson (Universität Hamburg) and Kate Yuan (Yale University): <q>Ethics is Failing Ethics: Citation Standards for Sexual Misconduct in the Field</q></p>
-              <p>Speaker: Neha Nandakumar (Simon Fraser University): <q>Taking Care of Yourself in a Workist Society</q></p>
-              <p>Moderator: Lilly Tran</p>
-            </div>
-          </div>
-
-
-          <div className="py-5">
-            <h2 className='text-1xl font-bold'>Panel G (3:00pm - 4:30pm ET) — <i>Historical Methodologies</i></h2>
-            <div className='pl-10'>
-              <p>Speaker: Jonmarc Bennett (Marquette University): <q>Tosaka Jun on Cultural Freedom</q></p>
-              <p>Speaker: Ethan Gettes (Fordham University): <q>The Fractured Totality: Benjamin, Hegel, and the Politics of Universal History</q></p>
-              <p>Speaker: Spencer Heitman (University of Arkansas): <q>Blaming Backward: On Moral Evaluations of Historical Predecessors</q></p>
-              <p>Moderator: Carson Saffold</p>
-            </div>
-          </div>
-
-          <div className="py-5">
-            <h2 className='text-1xl font-bold'>Closing Remarks (4:35pm - 4:45pm ET)</h2>
-            <div className='pl-10'>
-              <p>Closing remarks delivered by Philosophical Union of UNC Charlotte Co-Presidents</p>
-              <p>Nicholas Osaka and Steve Muir</p>
+              <p>Closing remarks delivered by Philosophical Union of UNC Charlotte Presidents</p>
+              <p>Brit Davis</p>
             </div>
           </div>
         </div>
